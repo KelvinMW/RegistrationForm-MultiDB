4c6b6bc0aa279b7ae0d8b46c67b7b7591d5df05b
Kelvin Maina <wkelvinmaina@gmail.com>
2024-07-06 11:33:43 +0300

    fix: date bug

--- fetchData.php
+++ fetchData.php
@@ -1,38 +1,42 @@
 <?php
 $databases = ['elm_secondary', 'elm_science-secondary'];
+
 function fetchData($startDate, $endDate, $database) {
-    require 'config.php';
+  require 'config.php';
 
-    $conn = new mysqli($host, $user, $password, $database);
+  $conn = new mysqli($host, $user, $password, $database);
 
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
 
-    $sql = "
+  $sql = "
     SELECT 
-        gp.officialName, gp.gender, gp.username, gp.dob, gp.email, gp.address1, gp.phone1, gp.countryOfBirth,
-        gp.emergency1Name, gp.emergency1Number1, gp.emergency2Name, gp.emergency2Number2,
-        gf.name as FormGroup, gy.name as YearGroup
+      gp.officialName, gp.gender, gp.username, 
+      DATE_FORMAT(gp.dateStart, '%Y-%m-%d') AS formattedDateStart, 
+      gp.email, gp.address1, gp.phone1, gp.countryOfBirth,
+      gp.emergency1Name, gp.emergency1Number1, gp.emergency2Name, gp.emergency2Number2,
+      gf.name as FormGroup, gy.name as YearGroup
     FROM gibbonPerson gp
     LEFT JOIN gibbonStudentEnrolment gse ON gp.gibbonPersonID = gse.gibbonPersonID
     LEFT JOIN gibbonFormGroup gf ON gse.gibbonFormGroupID = gf.gibbonFormGroupID
     LEFT JOIN gibbonYearGroup gy ON gse.gibbonYearGroupID = gy.gibbonYearGroupID
-    WHERE gp.dateStart BETWEEN ? AND ?
-";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param('ss', $startDate, $endDate);
-    $stmt->execute();
-    $result = $stmt->get_result();
+    WHERE DATE_FORMAT(gp.dateStart, '%Y-%m-%d') BETWEEN ? AND ?
+  ";
 
-    $data = [];
-    while ($row = $result->fetch_assoc()) {
-        $data[] = $row;
-    }
+  $stmt = $conn->prepare($sql);
+  $stmt->bind_param('ss', $startDate, $endDate);
+  $stmt->execute();
+  $result = $stmt->get_result();
 
-    $stmt->close();
-    $conn->close();
+  $data = [];
+  while ($row = $result->fetch_assoc()) {
+    $data[] = $row;
+  }
 
-    return $data;
+  $stmt->close();
+  $conn->close();
+
+  return $data;
 }
 ?>
